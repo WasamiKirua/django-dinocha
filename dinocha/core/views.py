db74140155b0be5c18ee14157508c886b008f03d
OasisTomato <122620587+OasisTomato@users.noreply.github.com>
2023-03-03 10:00:49 -0800

    shop page, urls and views.py

--- dinocha/core/views.py
+++ dinocha/core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def frontpage(request):
     return render(request, 'core/frontpage.html')
+
+def shop(request):
+    return render(request, 'core/shop.html')
--- dinocha/dinocha/urls.py
+++ dinocha/dinocha/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import frontpage
+from core.views import frontpage, shop
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('shop', shop, name='shop'),
     path('admin/', admin.site.urls),
 ]
